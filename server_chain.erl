9b7ee117c1ca8cd8d3845af8248d963d3431478a
Reese Letts <111404863+rkletts@users.noreply.github.com>
2025-02-26 19:00:24 -0500

    Update server_chain.erl

--- server_chain.erl
+++ server_chain.erl
@@ -1,80 +1,91 @@
--module(server_chain).
--export([start/0, serv1/1, serv2/1, serv3/1]).
+-module(chain).
+-export([start/0, serv1/0, serv2/0, serv3/1]).
 
 start() ->
-    Serv1 = spawn(?MODULE, serv1, [self()]),
+    Serv1 = spawn(chain, serv1, []),
     loop(Serv1).
 
 loop(Serv1) ->
-    io:format("Enter a message: "),
-    Msg = read_input(),
-    case Msg of
-        all_done -> ok;
-        _ -> 
-            Serv1 ! {self(), Msg},
+    io:format("Enter a message (or 'all_done' to exit): "),
+    Input = io:get_line(""),
+    Message = parse_input(Input),
+    case Message of
+        all_done ->
+            Serv1 ! halt,
+            io:format("Main function halting~n");
+        _ ->
+            Serv1 ! Message,
             loop(Serv1)
     end.
 
-read_input() ->
-    {ok, Tokens, _} = io:fread("", "~s"),
-    list_to_atom(hd(Tokens)).
+parse_input(Input) ->
+    % Here you would parse the input string into the appropriate message format
+    % For simplicity, let's assume the input is already in the correct format
+    list_to_atom(string:trim(Input)).
 
-serv1(Serv2) ->
-    Serv2_Pid = spawn(?MODULE, serv2, [self()]),
+serv1() ->
     receive
-        {_, {add, A, B}} ->
-            io:format("(serv1) ~p + ~p = ~p~n", [A, B, A + B]),
-            serv1(Serv2_Pid);
-        {_, {sub, A, B}} ->
-            io:format("(serv1) ~p - ~p = ~p~n", [A, B, A - B]),
-            serv1(Serv2_Pid);
-        {_, {mult, A, B}} ->
-            io:format("(serv1) ~p * ~p = ~p~n", [A, B, A * B]),
-            serv1(Serv2_Pid);
-        {_, {div, A, B}} when B =/= 0 ->
-            io:format("(serv1) ~p / ~p = ~p~n", [A, B, A / B]),
-            serv1(Serv2_Pid);
-        {_, {neg, A}} ->
-            io:format("(serv1) -~p = ~p~n", [A, -A]),
-            serv1(Serv2_Pid);
-        {_, {sqrt, A}} when A >= 0 ->
-            io:format("(serv1) sqrt(~p) = ~p~n", [A, math:sqrt(A)]),
-            serv1(Serv2_Pid);
-        {_, halt} ->
-            Serv2_Pid ! {self(), halt},
-            io:format("(serv1) Halting...~n");
+        {add, X, Y} ->
+            Result = X + Y,
+            io:format("(serv1) ~p + ~p = ~p~n", [X, Y, Result]),
+            serv1();
+        {sub, X, Y} ->
+            Result = X - Y,
+            io:format("(serv1) ~p - ~p = ~p~n", [X, Y, Result]),
+            serv1();
+        {mult, X, Y} ->
+            Result = X * Y,
+            io:format("(serv1) ~p * ~p = ~p~n", [X, Y, Result]),
+            serv1();
+        {div, X, Y} ->
+            Result = X / Y,
+            io:format("(serv1) ~p / ~p = ~p~n", [X, Y, Result]),
+            serv1();
+        {neg, X} ->
+            Result = -X,
+            io:format("(serv1) -~p = ~p~n", [X, Result]),
+            serv1();
+        {sqrt, X} ->
+            Result = math:sqrt(X),
+            io:format("(serv1) sqrt(~p) = ~p~n", [X, Result]),
+            serv1();
+        halt ->
+            io:format("(serv1) Halting~n"),
+            serv2();
         Msg ->
-            Serv2_Pid ! {self(), Msg},
-            serv1(Serv2_Pid)
+            Serv2 = spawn(chain, serv2, []),
+            Serv2 ! Msg,
+            serv1()
     end.
 
-serv2(Serv3) ->
-    Serv3_Pid = spawn(?MODULE, serv3, [0]),
+serv2() ->
     receive
-        {_, [H | T]} when is_integer(H) ->
+        [H | T] when is_integer(H) ->
             Sum = lists:sum([X || X <- [H | T], is_number(X)]),
             io:format("(serv2) Sum = ~p~n", [Sum]),
-            serv2(Serv3_Pid);
-        {_, [H | T]} when is_float(H) ->
-            Prod = lists:foldl(fun(X, Acc) when is_number(X) -> X * Acc; (_, Acc) -> Acc end, 1, [H | T]),
-            io:format("(serv2) Product = ~p~n", [Prod]),
-            serv2(Serv3_Pid);
-        {_, halt} ->
-            Serv3_Pid ! {self(), halt},
-            io:format("(serv2) Halting...~n");
+            serv2();
+        [H | T] when is_float(H) ->
+            Product = lists:foldl(fun(X, Acc) -> X * Acc end, 1, [X || X <- [H | T], is_number(X)]),
+            io:format("(serv2) Product = ~p~n", [Product]),
+            serv2();
+        halt ->
+            io:format("(serv2) Halting~n"),
+            Serv3 = spawn(chain, serv3, [0]),
+            Serv3 ! halt;
         Msg ->
-            Serv3_Pid ! {self(), Msg},
-            serv2(Serv3_Pid)
+            Serv3 = spawn(chain, serv3, [0]),
+            Serv3 ! Msg,
+            serv2()
     end.
 
-serv3(Count) ->
+serv3(UnprocessedCount) ->
     receive
-        {_, {error, Msg}} ->
+        {error, Msg} ->
             io:format("(serv3) Error: ~p~n", [Msg]),
-            serv3(Count);
-        {_, halt} ->
-            io:format("(serv3) Halting... Total unprocessed messages: ~p~n", [Count]);
+            serv3(UnprocessedCount);
+        halt ->
+            io:format("(serv3) Halting with ~p unprocessed messages~n", [UnprocessedCount]);
         Msg ->
             io:format("(serv3) Not handled: ~p~n", [Msg]),
-            serv3(Count + 1)
+            serv3(UnprocessedCount + 1)
     end.
